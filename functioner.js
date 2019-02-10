687cceab2f795a3b06622138b60cd7210c7dac5b
albmannen <32361715+albmannen@users.noreply.github.com>
2019-02-10 16:48:14 +0100

    Create functioner.js
    init

---
+++ functioner.js
@@ -0,0 +1,15 @@
+//var tal1,tal2;
+
+function Addera(tal1, tal2) {
+    sum = tal1 + tal2;
+    return sum;
+}
+
+function Multi(tal1, tal2) {
+    prod = tal1 * tal2;
+    return prod;
+}
+
+
+console.log (Addera(5, 5));
+console.log (Multi(5, 5));
